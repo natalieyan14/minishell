1631c00d4dde3d0c070d18f040edba1caa38b62b
Natalie Harutyunyan <nataharutyunyan6@gmail.com>
2025-11-11 16:23:35 +0400

    last

--- Makefile
+++ Makefile
@@ -11,9 +11,9 @@ SRC = sourses/tokenize.c sourses/tokenize_utils.c sourses/tokenize_handlers.c so
       sourses/env.c sourses/env_2.c sourses/main.c sourses/pars.c sourses/pars2.c sourses/pars3.c  \
       sourses/pars_cmd.c sourses/pars_redir.c sourses/pars_utils.c \
       sourses/utils.c sourses/utils_2.c sourses/utils_3.c sourses/cmd_exec.c sourses/exec_cmd.c sourses/bulitin.c sourses/bulitin_2.c \
-      sourses/dollar.c sourses/dollar_utils.c sourses/heredoc.c sourses/heredoc_utils.c sourses/redirect.c sourses/redirect2.c sourses/redirect3.c sourses/redirect4.c sourses/redirect5.c sourses/pipe.c sourses/pipe_utils.c sourses/exec_external.c sourses/exec_single.c sourses/pipe_exec.c sourses/signals.c \
+	sourses/dollar.c sourses/dollar_utils.c sourses/heredoc.c sourses/heredoc_utils.c sourses/redirect.c sourses/redirect3.c sourses/redirect4.c sourses/redirect5.c sourses/pipe.c sourses/pipe_utils.c sourses/exec_external.c sourses/exec_single.c sourses/pipe_exec.c sourses/signals.c \
       sourses/signals2.c sourses/exit_status.c sourses/builtin_3.c sourses/builtin_4.c sourses/builtin_utils.c sourses/builtin_basic.c sourses/exec_utils.c sourses/unset_utils.c sourses/export_utils.c \
-	  sourses/main_utils1.c sourses/main_utils2.c sourses/main_utils3.c
+	  sourses/main_utils1.c sourses/main_utils2.c sourses/main_utils3.c sourses/redirect_helper.c
 
 OBJ = $(SRC:.c=.o)
 
--- sourses/redirect5.c
+++ sourses/redirect5.c
@@ -6,11 +6,63 @@
 /*   By: natalieyan <natalieyan@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 01:50:40 by natalieyan        #+#    #+#             */
-/*   Updated: 2025/11/11 15:15:51 by natalieyan       ###   ########.fr       */
+/*   Updated: 2025/11/11 16:12:05 by natalieyan       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
+
+static int	process_output_redir(t_ordered_redir *redir)
+{
+	int	fd;
+	int	flags;
+
+	flags = O_CREAT | O_WRONLY;
+	if (redir->type == REDIR_APPEND)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	fd = open(redir->filename, flags, 0644);
+	if (fd < 0)
+	{
+		perror(redir->filename);
+		set_exit_status(1);
+		return (-1);
+	}
+	if (dup2(fd, STDOUT_FILENO) < 0)
+	{
+		perror("dup2");
+		close(fd);
+		set_exit_status(1);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
+
+static int	process_input_redir(t_ordered_redir *redir)
+{
+	int	fd;
+
+	fd = open(redir->filename, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(redir->filename);
+		set_exit_status(1);
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
+
+int	process_single_redir(t_ordered_redir *redir)
+{
+	if (redir->type == REDIR_OUTPUT || redir->type == REDIR_APPEND)
+		return (process_output_redir(redir));
+	else if (redir->type == REDIR_INPUT)
+		return (process_input_redir(redir));
+	return (0);
+}
 
 int	validate_helper(t_ordered_redir *redirs)
 {
@@ -30,108 +82,6 @@ int	validate_helper(t_ordered_redir *redirs)
 				return (-1);
 			}
 			close(fd);
-		}
-		curr = curr->next;
-	}
-	return (0);
-}
-
-static int	validate_output_redir(t_ordered_redir *curr)
-{
-	int	fd;
-	int	flags;
-
-	flags = O_CREAT | O_WRONLY;
-	if (curr->type == REDIR_APPEND)
-		flags |= O_APPEND;
-	else
-		flags |= O_TRUNC;
-	fd = open(curr->filename, flags, 0644);
-	if (fd < 0)
-	{
-		perror(curr->filename);
-		set_exit_status(1);
-		return (-1);
-	}
-	close(fd);
-	return (0);
-}
-
-static int	validate_input_redir(t_ordered_redir *curr)
-{
-	int	fd;
-
-	fd = open(curr->filename, O_RDONLY);
-	if (fd < 0)
-	{
-		perror(curr->filename);
-		set_exit_status(1);
-		return (-1);
-	}
-	close(fd);
-	return (0);
-}
-
-int	validate_setup_helper(t_ordered_redir *redirs)
-{
-	t_ordered_redir	*curr;
-
-	curr = redirs;
-	while (curr)
-	{
-		if (curr->type == REDIR_INPUT)
-		{
-			if (validate_input_redir(curr) < 0)
-				return (-1);
-		}
-		else
-		{
-			if (validate_output_redir(curr) < 0)
-				return (-1);
-		}
-		curr = curr->next;
-	}
-	return (0);
-}
-
-static int	setup_input_redir(t_ordered_redir *curr)
-{
-	int	fd;
-
-	fd = open(curr->filename, O_RDONLY);
-	if (fd < 0)
-	{
-		perror(curr->filename);
-		set_exit_status(1);
-		return (-1);
-	}
-	if (dup2(fd, STDIN_FILENO) < 0)
-	{
-		perror("dup2");
-		close(fd);
-		set_exit_status(1);
-		return (-1);
-	}
-	close(fd);
-	return (0);
-}
-
-int	setup_helper(t_ordered_redir *redirs)
-{
-	t_ordered_redir	*curr;
-
-	curr = redirs;
-	while (curr)
-	{
-		if (curr->type == REDIR_INPUT)
-		{
-			if (setup_input_redir(curr) < 0)
-				return (-1);
-		}
-		else if (curr->type == REDIR_OUTPUT || curr->type == REDIR_APPEND)
-		{
-			if (process_single_redir(curr) < 0)
-				return (-1);
 		}
 		curr = curr->next;
 	}
